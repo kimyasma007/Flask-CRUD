6367b543c2879d98ca7769c28c87af1d25c1c43e
Kimyasma007 <sodeeqbigy@gmail.com>
2023-03-26 00:35:14 +0100

    Modify app.py

--- app.py
+++ app.py
@@ -1,10 +1,36 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 
 
 
 
 app = Flask (__name__)
+app.secret_key = "Kimyasma"
+
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:''@localhost/crud'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class Data(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    email = db.Column(db.String(100))
+    phone = db.Column(db.String(100))
+
+    def __init__(self, name, email, phone):
+        self.name = name
+        self.email = email
+        self.phone = phone
+
+with app.app_context():
+    db.create_all()
+
+
 @app.route('/')
 def index ():
     return render_template("index.html")
