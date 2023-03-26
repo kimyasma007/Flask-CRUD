d2a2d007fee37245943ec3ba98c4a77c452f2b00
Kimyasma007 <sodeeqbigy@gmail.com>
2023-03-26 12:52:08 +0100

    Modify app & index

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 
@@ -29,6 +29,19 @@ with app.app_context():
 def index ():
     return render_template("index.html")
 
+@app.route('/insert', methods = ['POST'])
+def insert():
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        phone = request.form['phone']
+
+
+        my_data = Data(name, email, phone)
+        db.session.add(my_data)
+        db.session.commit()
+
+        return redirect (url_for('Index'))
 
 
 
